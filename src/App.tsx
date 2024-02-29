38dc6482774f18322a67d06aaf85ff8ba40e315b
ADJE Abed <77735946+Abed-Nego28@users.noreply.github.com>
2024-02-29 17:18:37 +0100

    modify and update list

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { SafeAreaView, KeyboardAvoidingView, Platform } from 'react-native';
-import Login from './screens/LoginForm';
+import RestaurantList from './screens/RestaurantList'; 
 
 const App: React.FC = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
-        <Login />
-      </KeyboardAvoidingView>
+       <RestaurantList />
+      {/* <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
+        {}
+      </KeyboardAvoidingView> */}
     </SafeAreaView>
   );
 };
 
 export default App;
+
---
+++ src/screens/RestaurantList.jsx
@@ -0,0 +1,228 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+//import MapView, { Marker } from 'react-native-maps';
+//import MapComponent from './MapComponent';
+
+import { PermissionsAndroid, Platform } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
+
+
+const RestaurantList = () => {
+  
+    const [restaurants, setRestaurants] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [selectedType, setSelectedType] = useState('all');
+    const [userLocation, setUserLocation] = useState({ });
+    const [isFavorite, setIsFavorite] = useState(false);
+    const requestLocationPermission = async () => {
+      try {
+        if (Platform.OS === 'android') {
+          const granted = await PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+          ]);
+          
+          if (
+            granted['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED &&
+            granted['android.permission.ACCESS_COARSE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED
+          ) {
+            console.log('Autorisation accordée');
+            // Vous pouvez maintenant récupérer la géolocalisation
+            Geolocation.getCurrentPosition(
+              (position) => {
+                setUserLocation({ latitude: position.coords.latitude, longitude: position.coords.longitude })
+              },
+              (error) => {
+                console.error('Erreur de géolocalisation:', error);
+              },
+              { enableHighAccuracy: true, timeout: 150000, maximumAge: 10000 }
+            );
+          } else {
+            Alert.alert(
+              'Permission refusée',
+              'Cette application a besoin d\'accéder à votre localisation pour fonctionner.',
+              [{ text: 'OK', onPress: () => console.log('OK Pressed') }]
+            );
+          }
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    };
+
+    // use user's location and ask for permission
+
+    const fetchRestaurants = async () => {
+      const API_KEY = 'fsq3zYYs8PG4sj2b6qC+h+63+11H8KZkdNn94kI3Mrc+9i8=';
+      const API_URL = 'https://api.foursquare.com/v3/places/search';
+
+      try {
+        const searchParams = new URLSearchParams({
+          query: searchQuery, 
+          ll: userLocation ? `${userLocation.latitude},${userLocation.longitude}` : '48.8566,2.3522',
+          //open_now: 'true',
+          sort: 'DISTANCE',
+          limit: '25',
+          categoryId: selectedType === 'all' ? '' : selectedType ,
+        });
+
+        const results = await fetch(
+          `${API_URL}?${searchParams}`,
+          {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              Authorization: API_KEY,
+            }
+          }
+        );
+
+        const data = await results.json();
+        if (data && data.results) {
+          const extractedRestaurants = await Promise.all(data.results.map(async (result) => {
+            const photosResponse = await fetch(
+              `https://api.foursquare.com/v3/places/${result.fsq_id}/photos`,
+              {
+                method: 'GET',
+                headers: {
+                  Accept: 'application/json',
+                  Authorization: API_KEY,
+                }
+              }
+            );
+
+            const photosData = await photosResponse.json();
+            const photoURLs = photosData.map(item => item.prefix + 'original' + item.suffix);
+            const firstPhotoURL = photoURLs[0];
+            const remainingPhotoURLs = photoURLs.slice(1);
+            console.log(result);
+            return {
+              id: result.fsq_id,
+              name: result.name,
+              address: result.location.formatted_address,
+              distance: result.distance,
+              categories: result.categories,
+              status: result.closed_bucket,
+              city: result.location.locality,
+              country: result.location.country,
+              region: result.location.region,
+              url: firstPhotoURL,
+              // urls: remainingPhotoURLs
+            };
+          }));
+
+          setRestaurants(extractedRestaurants);
+        } else {
+          console.error('Response format is incorrect:', data);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }; 
+  useEffect(() => {
+    requestLocationPermission()
+}, []); 
+
+  const renderRestaurantItem = ({ item }) => (
+    <View style={styles.restaurantItem}>
+      <Text style={styles.restaurantName}>
+        {item.name}
+        {item.isFavorite && <MaterialIcons name="favorite" size={24} color="red" style={styles.favoriteIcon} />}
+      </Text>
+      <Text style={styles.restaurantAddress}>{item.city}</Text>
+      <Text style={styles.restaurantDistance}>{item.distance/1000} kms away</Text>
+      <Image source={{ uri: item.url }} style={styles.image} />
+      {/* Bouton de favoris */}
+      <TouchableOpacity onPress={() => toggleFavorite(item.id)}>
+        <Text style={styles.favoriteButton}>{item.isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search for restaurants..."
+        onChangeText={(text) => setSearchQuery(text)}
+        onEndEditing={fetchRestaurants}
+      />
+      <FlatList
+        data={restaurants}
+        keyExtractor={(restaurant) => restaurant.id}
+        renderItem={renderRestaurantItem}
+        style={{flex:1}}
+      />
+    
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    paddingHorizontal: 10,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  restaurantItem: {
+    marginBottom: 20,
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  restaurantName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#333',
+  },
+  restaurantAddress: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 5,
+  },
+  restaurantDistance: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 10,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 8,
+  },
+  favoriteButton: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'blue',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  favoriteIcon: {
+    marginLeft: 5,
+  },
+
+  
+});
+
+export default RestaurantList;
+
